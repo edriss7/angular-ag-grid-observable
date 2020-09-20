0b44f57b1a69f8babecb40454b33df14cd4bdca7
edris <edriss7@yahoo.com>
2020-09-20 10:06:15 -0700

    Added ad-grid and hooked it up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MockTestService } from './mockTest.service'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-ag-grid-observable';
+	constructor(private mockTest: MockTestService) { }
+
+	title = 'angular-ag-grid-observable';
+
+	columnDefs = [
+	    {headerName: 'Make', field: 'make', sortable: true, filter: true},
+	    {headerName: 'Model', field: 'model', sortable: true, filter: true},
+	    {headerName: 'Price', field: 'price', sortable: true, filter: true}
+	];
+
+	rowData = [
+	    { make: 'Toyota', model: 'Celica', price: 35000 },
+	    { make: 'Ford', model: 'Mondeo', price: 32000 },
+	    { make: 'Porsche', model: 'Boxter', price: 72000 }
+	];
 }
