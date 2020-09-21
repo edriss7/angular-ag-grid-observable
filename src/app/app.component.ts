acfad05c575d2aeedb71e67943bca45866a5aaa4
edris <edriss7@yahoo.com>
2020-09-21 06:35:48 -0700

    Added the data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { MockTestService } from './mockTest.service'
+import { MockService } from './mock.service'
+import { timeout } from 'rxjs/operators';
+import { Asset } from './asset';
+import {GridOptions, AllCommunityModules} from "@ag-grid-community/all-modules";
 
 @Component({
   selector: 'app-root',
@@ -7,19 +10,71 @@ import { MockTestService } from './mockTest.service'
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-	constructor(private mockTest: MockTestService) { }
-
 	title = 'angular-ag-grid-observable';
 
 	columnDefs = [
-	    {headerName: 'Make', field: 'make', sortable: true, filter: true},
-	    {headerName: 'Model', field: 'model', sortable: true, filter: true},
-	    {headerName: 'Price', field: 'price', sortable: true, filter: true}
+	    {headerName: 'Id', field: 'id', sortable: true, filter: true},
+	    {headerName: 'Asset Name', field: 'assetName', sortable: true, filter: true},
+	    {headerName: 'Price', field: 'price', sortable: true, filter: true},
+	    {headerName: 'LastUpdate', field: 'lastUpdate', sortable: true, filter: true},
+	    {headerName: 'Type', field: 'type', sortable: true, filter: true}
 	];
 
-	rowData = [
-	    { make: 'Toyota', model: 'Celica', price: 35000 },
-	    { make: 'Ford', model: 'Mondeo', price: 32000 },
-	    { make: 'Porsche', model: 'Boxter', price: 72000 }
-	];
+	rowData : Asset[]= [];
+	gridOptions: GridOptions;
+
+	constructor(private mock: MockService) { 
+		// const obs = this.mock.mock().subscribe(  
+		// 	x => {
+		// 			// console.log('Observer got a next value: ' , x);
+		// 			this.rowData.push(x);
+		// 	});
+		// 	setTimeout(() => {
+		// 	    obs.unsubscribe();
+		// 	}, 1000);
+
+
+		this.gridOptions = <GridOptions> {
+            enableRangeSelection: true,
+            columnDefs: this.columnDefs,
+            getRowNodeId: function (data) {
+                // the code is unique, so perfect for the id
+                return data.id;
+            },
+            onGridReady: () => {
+                const obs = this.mock.mock().subscribe(
+                    rowData => {
+                        // the initial full set of data
+                        // note that we don't need to un-subscribe here as it's a one off data load
+                        if (this.gridOptions.api) { // can be null when tabbing between the examples
+                            // this.gridOptions.api.setRowData([rowData]);
+                            this.gridOptions.api.updateRowData({add: [rowData]});
+                        }
+                    }
+                );
+                setTimeout(() => {
+				    obs.unsubscribe();
+				}, 1000);
+            },
+
+            onFirstDataRendered(params) {
+                params.api.sizeColumnsToFit();
+            }
+        };
+
+    }
+
+	// ngOnInit() {}
+
+	// columnDefs = [
+	//     {headerName: 'Make', field: 'make', sortable: true, filter: true},
+	//     {headerName: 'Model', field: 'model', sortable: true, filter: true},
+	//     {headerName: 'Price', field: 'price', sortable: true, filter: true}
+	// ];
+
+	// rowData = [
+	//     { make: 'Toyota', model: 'Celica', price: 35000 },
+	//     { make: 'Ford', model: 'Mondeo', price: 32000 },
+	//     { make: 'Porsche', model: 'Boxter', price: 72000 }
+	// ];
 }
